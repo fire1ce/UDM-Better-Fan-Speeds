ac52142a808484d9913df2674048f020bbd4f6bf
Stas Yakobov <stas@3os.org>
2022-07-23 17:03:44 +0300

    init

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ curl -sO https://raw.githubusercontent.com/fire1ce/UDM-Better-Fan-Speeds/main/11
 mv 11-udm-better-fan-speed.sh /mnt/data/on_boot.d/11-udm-better-fan-speed.sh
 chmod +x /mnt/data/on_boot.d/11-udm-better-fan-speed.sh
 /mnt/data/on_boot.d/11-udm-better-fan-speed.sh
-echo "11-udm-better-fan-speed.sh installed '\n'"
-echo "You can eddit the fan-speed settings in /mnt/data/on_boot.d/11-udm-better-fan-speed.sh \n"
-
+echo "11-udm-better-fan-speed.sh installed successfully"
+echo "You can eddit the fan-speed settings in /mnt/data/on_boot.d/11-udm-better-fan-speed.sh"
 exit 0
